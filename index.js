92ff0f033da5f1ccdbdbff994a97f335a739ed9e
hernando <hernandoram1998@gmail.com>
2020-08-18 02:46:11 -0500

    make it better

--- index.js
+++ index.js
@@ -174,10 +174,11 @@ let paginaInicio = `<!DOCTYPE html>
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                  <a class="dropdown-item" href="#">
+                  <a class="dropdown-item" href="/index.html">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
+                    Salir
                   </a>
+                  <!--
                   <a class="dropdown-item" href="#">
                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                     Settings
@@ -191,6 +192,7 @@ let paginaInicio = `<!DOCTYPE html>
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                     Logout
                   </a>
+                  -->
                 </div>
               </li>
   
