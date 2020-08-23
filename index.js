5dc9e4dd342318a37cb71a74784f49d312bf44b4
hernando <hernandoram1998@gmail.com>
2020-08-23 09:10:15 -0500

    make it better

--- index.js
+++ index.js
@@ -208,7 +208,7 @@ let paginaInicio = `<!DOCTYPE html>
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                  <a class="dropdown-item" href="/index.html">
+                  <a class="dropdown-item" href="javascript:cerrarSesion()">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Salir
                   </a>
@@ -517,7 +517,7 @@ let paginaInicioIndex=`<!DOCTYPE html>
                 </a>
                 <!-- Dropdown - User Information -->
                 <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                  <a class="dropdown-item" href="/index.html">
+                  <a class="dropdown-item" href="javascript:cerrarSesion()">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Salir
                   </a>
--- public/plantilla/js/main.js
+++ public/plantilla/js/main.js
@@ -61,6 +61,14 @@ function activar(a) {
         });
 }
 
+function cerrarSesion(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+        location.href("/index.html");
+      }).catch(function(error) {
+        // An error happened.
+      });
+}
 function mostrarPrueba(){
 
     firebase.auth().onAuthStateChanged(function(user) {
